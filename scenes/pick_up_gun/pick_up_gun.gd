c0f1e552e7c2e8f21c229e59371cac4e5f47ef03
Franco Noronha <fnpereira@inf.ufpel.edu.br>
2024-06-20 21:51:12 -0300

    cleanup´

--- scenes/pick_up_gun/pick_up_gun.gd
+++ scenes/pick_up_gun/pick_up_gun.gd
@@ -1,40 +1,40 @@
 extends Node2D
 
-var players_in_area = []
-var state = "spawned"
-@export var item_scene = "res://scenes/guns/shotgun/shotgun.tscn"
+var players_in_area: Array[Node2D] = []
+var state := "spawned"
+@export var item_scene := "res://scenes/guns/shotgun/shotgun.tscn"
 
-func _ready():
+
+func _ready() -> void:
 	pass
 
-func _process(delta):
+
+func _process(_delta) -> void:
 	if state == "spawned":
 		check_for_player_interaction()
 		
 
 
-func check_for_player_interaction():
+func check_for_player_interaction() -> void:
 	for player in players_in_area:
 		if player.is_interacting():
 			rpc("player_interaction")
 			player.GunManager.rpc("equip_gun", item_scene)
 			break
 
+
 @rpc("any_peer", "call_local")
-func player_interaction():
+func player_interaction() -> void:
 	state = "picked_up"
 	players_in_area.clear()
 	queue_free()
 
-func _on_pickable_area_body_entered(body):
-	print(body)
+
+func _on_pickable_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		players_in_area.append(body)
-		print("entroo")
 
 
-func _on_pickable_area_body_exited(body):
-	print("saiu")
+func _on_pickable_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		players_in_area.erase(body)
-		print("saiuu")
